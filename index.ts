5b6a13dd9794334e28fb42df4e911786be5de819
Jeremy Scott <jeremy@jeremyscott.io>
2022-10-22 14:27:58 +1100

    completed tutorial

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Context, APIGatewayProxyCallback, APIGatewayEvent,APIGatewayProxyResult } from 'aws-lambda';
 
-export const lambdaHandler = async (event:APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
+export const handler = async (event:APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
     return{
         statusCode: 200,
         body: JSON.stringify({
